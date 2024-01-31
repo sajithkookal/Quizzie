9d0ce27650cd16a97ac5f191800f1493418ec2ec
sajith <sajithkookal7@gmail.com>
2024-02-01 04:05:29 +0530

    final

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
 export const backendBaseUrl = "https://quizzie-d5my.onrender.com";
-export const frontEndBaseUrl = "http://localhost:3000";
+export const frontEndBaseUrl = "https://quizzie-ebon.vercel.app";
